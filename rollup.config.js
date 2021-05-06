84c538c7e67dade0143eb2f37e05058d85cdd1c8
Filip Johansson <filip.johansson@protonmail.com>
2021-05-06 16:09:04 +0200

    chore: maintenance

--- rollup.config.js
+++ rollup.config.js
@@ -1,25 +1,16 @@
-import babel from "rollup-plugin-babel";
-import resolve from "rollup-plugin-node-resolve";
-import commonjs from "rollup-plugin-commonjs";
-import replace from "@rollup/plugin-replace";
-import peerDepsExternal from "rollup-plugin-peer-deps-external";
-
-const NODE_ENV = process.env.NODE_ENV || "development";
-const outputFile = NODE_ENV === "production" ? "./lib/prod.js" : "./lib/dev.js";
+import babel from "@rollup/plugin-babel";
+import resolve from "@rollup/plugin-node-resolve";
+import commonjs from "@rollup/plugin-commonjs";
 
 const extensions = [".js"];
 
 export default {
   input: "./src/index.js",
   output: {
-    file: outputFile,
+    file: "./lib/index.js",
     format: "cjs",
   },
   plugins: [
-    peerDepsExternal(),
-    replace({
-      "process.env.NODE_ENV": JSON.stringify(NODE_ENV),
-    }),
     babel({
       exclude: "node_modules/**",
       presets: [
