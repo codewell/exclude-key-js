edd5a4aea618e237331e50fcdbc12b4ca8f7b27d
Filip Johansson <filip.johansson@protonmail.com>
2021-05-06 16:09:04 +0200

    chore: maintenance

--- tests/index.test.js
+++ tests/index.test.js
@@ -1,47 +1,46 @@
-const devLib = require("../lib/dev");
-const prodLib = require("../lib/prod");
+const lib = require("../lib/index.js");
 
 test("No arguments should throw", () => {
   expect(() => {
-    devLib();
+    lib();
   }).toThrow();
 });
 
 test("Empty object no key", () => {
-  expect(devLib({})).toEqual({});
+  expect(lib({})).toEqual({});
 });
 
 test("Empty object empty key", () => {
-  expect(devLib({}, "")).toEqual({});
+  expect(lib({}, "")).toEqual({});
 });
 
 test("Empty object key", () => {
-  expect(devLib({}, "foo")).toEqual({});
+  expect(lib({}, "foo")).toEqual({});
 });
 
 test("{foo: 'bar'}, '' => {foo: 'bar'}", () => {
-  expect(devLib({ foo: "bar" }, "")).toEqual({ foo: "bar" });
+  expect(lib({ foo: "bar" }, "")).toEqual({ foo: "bar" });
 });
 
 test("{foo: 'bar'}, 'foo' => {}", () => {
-  expect(devLib({ foo: "bar" }, "foo")).toEqual({});
+  expect(lib({ foo: "bar" }, "foo")).toEqual({});
 });
 
 test("{ one: 1, two: 2, three: 3 }, 'two' => { one: 1, three: 3 }", () => {
-  expect(devLib({ one: 1, two: 2, three: 3 }, "two")).toEqual({
+  expect(lib({ one: 1, two: 2, three: 3 }, "two")).toEqual({
     one: 1,
     three: 3,
   });
 });
 
 test("{ one: 1, two: 2, three: 3 }, 'two', 'three' => { one: 1, three: 3 }", () => {
-  expect(devLib({ one: 1, two: 2, three: 3 }, "two", "three")).toEqual({
+  expect(lib({ one: 1, two: 2, three: 3 }, "two", "three")).toEqual({
     one: 1,
   });
 });
 
 test("{ one: 1, two: 2, three: 3 } should throw", () => {
   expect(() => {
-    devLib({ one: 1, two: 2, three: 3 }, "two", 1);
+    lib({ one: 1, two: 2, three: 3 }, "two", 1);
   }).toThrow();
 });
